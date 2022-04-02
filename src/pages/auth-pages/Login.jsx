1a33d4e7b3063087d964c1489ebe3a2808fd4682
Chirag Gupta <51075671+Hardiegogo@users.noreply.github.com>
2022-04-02 15:25:00 +0530

    refactor

--- src/pages/auth-pages/Login.jsx
+++ src/pages/auth-pages/Login.jsx
@@ -1,50 +1,63 @@
-import React from 'react'
-import { useState } from 'react'
-import axios from 'axios'
-import './auth.css'
-import { useAuth } from '../../context/useAuth'
-import { Link, useNavigate } from 'react-router-dom'
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import "./auth.css";
+import { useAuth } from "../../context/useAuth";
+import { Link, useNavigate } from "react-router-dom";
+import { loginUser } from "../../utils/auth-utils/loginUser";
 
-const Login=()=>{
+const Login = () => {
+  const { authState, dispatchAuth } = useAuth();
 
-    const {authState,dispatchAuth}=useAuth()
-
-    const [user,setUser]=useState({email:'',password:''})
-    const navigate=useNavigate()
-    const changeHandler=(e)=>{
-        setUser({...user,[e.target.name]:e.target.value})
-    }
-
+  const [user, setUser] = useState({ email: "", password: "" });
+  const navigate = useNavigate();
+  const changeHandler = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
 
   return (
-      <div className="content-auth">
-          <div className="auth-container card-shadow p-24">
-                
-                    <h3 className="center-text">Login</h3>
-                    <label for="username">
-                        <h4 className="mt-16">Email:</h4>
-                        <input className="input input-primary" type="email" name="email" value={user.email} onChange={changeHandler}/>
-                    </label>
-                    <h4 className="mt-16">Password:</h4><label>
-                    <input className="input input-primary" id="user-password" name="password" type="password" value={user.password} onChange={changeHandler}/>
-                    </label>
-                    <div className="auth_options mt-16">
-                        <label for="remember-me x-small-text">
-                            <input type="checkbox" id="remember-me"/>
-                            I accept all terms and conditions.
-                        </label>
-
-
-                    </div>
-                    <button className="btn btn-primary mt-16" onClick={()=>loginUser(user,authState,dispatchAuth,navigate)}>Login</button>
-                    <Link to='/signup' className="link-secondary center-text mt-1">Create a new account {'>'}</Link>
-                
-
-
-    </div>
+    <div className="content-auth">
+      <div className="auth-container card-shadow p-24">
+        <h3 className="center-text">Login</h3>
+        <label for="username">
+          <h4 className="mt-16">Email:</h4>
+          <input
+            className="input input-primary"
+            type="email"
+            name="email"
+            value={user.email}
+            onChange={changeHandler}
+          />
+        </label>
+        <h4 className="mt-16">Password:</h4>
+        <label>
+          <input
+            className="input input-primary"
+            id="user-password"
+            name="password"
+            type="password"
+            value={user.password}
+            onChange={changeHandler}
+          />
+        </label>
+        <div className="auth_options mt-16">
+          <label for="remember-me x-small-text">
+            <input type="checkbox" id="remember-me" />I accept all terms and
+            conditions.
+          </label>
+        </div>
+        <button
+          className="btn btn-primary mt-16"
+          onClick={() => loginUser(user, authState, dispatchAuth, navigate)}
+        >
+          Login
+        </button>
+        <Link to="/signup" className="link-secondary center-text mt-1">
+          Create a new account {">"}
+        </Link>
       </div>
-    
-  )
-}
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/home-page/Homepage.jsx
+++ src/pages/home-page/Homepage.jsx
@@ -1,63 +1,64 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import { useReducer } from 'react';
-import './Homepage.css'
-import { useEffect } from 'react';
-import axios from 'axios';
-import CategoryCard from '../../components/category-card-component/CategoryCard';
-import ProductCard from '../../components/product-card-component/ProductCard';
-import { dataReducer } from '../../reducers/dataReducer';
-import { loadData } from '../../utils/loadData';
-import { useProducts } from '../../context/useProducts';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useReducer } from "react";
+import "./Homepage.css";
+import { useEffect } from "react";
+import axios from "axios";
+import CategoryCard from "../../components/category-card-component/CategoryCard";
+import ProductCard from "../../components/product-card-component/ProductCard";
+import { dataReducer } from "../../reducers/dataReducer";
+import { loadData } from "../../utils/loadData";
+import { useProducts } from "../../context/useProducts";
 
-const Homepage= () => {
-  
-  const {state,dispatch}=useProducts()
+const Homepage = () => {
+  const { state, dispatch } = useProducts();
 
-  useEffect(()=>{
-    loadData(dispatch,'products');
-    loadData(dispatch,'categories')
-  },[])
-
-
+  useEffect(() => {
+    loadData(dispatch, "products");
+    loadData(dispatch, "categories");
+  }, []);
 
   return (
-      <main className='content p-64'> 
-
-        <div className="hero grid-2">
-
-                <div className="hero-left">
-                    <div className="hero-left-content">
-                        <h1>One place <br/> for all your <br/> <span className="prime-heading">Anime</span>-wishes.</h1>
-                        <Link  to="/products" className='btn-large btn-primary btn-hero mt-24 medium-text'>Go to products</Link>
-                    </div>
-
-                </div>
-                <div className="hero-right">
-                    <img src="https://res.cloudinary.com/dqqehaaqo/image/upload/v1647734809/hello_o63naq.png" className="resp-img" alt=""/>
-
-                </div>
-
+    <main className="content p-64">
+      <div className="hero grid-2">
+        <div className="hero-left">
+          <div className="hero-left-content">
+            <h1>
+              One place <br /> for all your <br />{" "}
+              <span className="prime-heading">Anime</span>-wishes.
+            </h1>
+            <Link
+              to="/products"
+              className="btn-large btn-primary btn-hero mt-24 medium-text"
+            >
+              Go to products
+            </Link>
+          </div>
         </div>
-
-        <h2 className="mt-64">Categories</h2>
-        <div  className="categories-list grid-auto-fit grid-category mt-24">
-        { state.categories.map((category)=>
-          <CategoryCard category={category} key={category._id}/>
-          )}
+        <div className="hero-right">
+          <img
+            src="https://res.cloudinary.com/dqqehaaqo/image/upload/v1647734809/hello_o63naq.png"
+            className="resp-img"
+            alt=""
+          />
         </div>
+      </div>
 
-        <h2 class="mt-64">Best sellers</h2>
-        <div class="product-list grid-auto-fit mt-24">
-        {state.products.map((product)=>
-        <ProductCard product={product} key={product._id}/>)
-        }
-        
-        </div>
-      </main>
-      
-    
-  )
-}
+      <h2 className="mt-64">Categories</h2>
+      <div className="categories-list grid-auto-fit grid-category mt-24">
+        {state.categories.map((category) => (
+          <CategoryCard category={category} key={category._id} />
+        ))}
+      </div>
+
+      <h2 class="mt-64">Best sellers</h2>
+      <div class="product-list grid-auto-fit mt-24">
+        {state.products.map((product) => (
+          <ProductCard product={product} key={product._id} />
+        ))}
+      </div>
+    </main>
+  );
+};
 
 export default Homepage;
